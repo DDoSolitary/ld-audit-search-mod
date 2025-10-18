05e9b307cc0507d0c20ce38511773e132b35bfc1
Hoang Nguyen <folliekazetani@protonmail.com>
2025-10-18 14:09:03 +0700

    remove flake-utils from flake inputs

--- flake.nix
+++ flake.nix
@@ -1,28 +1,45 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs = { self, flake-utils, nixpkgs, ... }:
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
     let
-      myOverlay = import ./overlay.nix;
-    in flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "i686-linux"
-      "aarch64-linux"
-    ] (system: let
-      pkgs = import nixpkgs { inherit system; overlays = [ myOverlay ]; };
       name = "ld-audit-search-mod";
-    in {
-      packages = rec {
+
+      myOverlay = import ./overlay.nix;
+
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "i686-linux"
+            "aarch64-linux"
+          ]
+          (
+            system:
+            f (
+              import nixpkgs {
+                inherit system;
+                overlays = [ myOverlay ];
+              }
+            )
+          );
+    in
+    {
+      packages = eachSystem (pkgs: rec {
         default = pkgs.${name};
         ${name} = default;
-      };
-      devShells.default = (pkgs.mkShell.override { stdenv = pkgs.${name}.stdenv; }) {
-        inputsFrom = [ pkgs.${name} ];
-        packages = [ pkgs.clang-tools ];
-      };
-    }) // {
+      });
+
+      devShells.default = eachSystem (
+        pkgs:
+        (pkgs.mkShell.override { stdenv = pkgs.${name}.stdenv; }) {
+          inputsFrom = [ pkgs.${name} ];
+          packages = [ pkgs.clang-tools ];
+        }
+      );
+
       overlays.default = myOverlay;
     };
 }
